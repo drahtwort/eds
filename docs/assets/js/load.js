37011a9c08a78f2ffe2479926b1c6adc5da4a9cd
Don Heartfield <heartfield@drahtwort.org>
2019-03-30 11:50:13 +0100

    script updated

--- docs/assets/js/load.js
+++ docs/assets/js/load.js
@@ -3,13 +3,6 @@ const endpoint = "https://miscella.github.io/txt/"
 function loadTextContent(textId) {
     var url = endpoint + textId + ".txt";
     var xhttp;
-    /*
-    var urlParts = url.replace(".txt","").split("/");
-    var fileId = urlParts[urlParts.length-1];
-    console.log(fileId);
-    */
-
-    /* console.log(urlParts.length); */
     xhttp=new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
