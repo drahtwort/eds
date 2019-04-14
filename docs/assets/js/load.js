f8268f591dee72d7b061568572d7110cacaec502
Don Heartfield <heartfield@drahtwort.org>
2019-04-14 11:52:23 +0200

    script and style updated

--- docs/assets/js/load.js
+++ docs/assets/js/load.js
@@ -2,19 +2,37 @@
 const endpoint = "https://drahtwort.github.io/plain/"
 
 function loadTextContent(textId) {
-    var dir = textId.substr(0,textId.length-4).replace(/\./g,"/") + "/";
+    document.getElementById(textId).onclick = '';
+    var spanId = textId.split(".").slice(0,2).join(".");
+    var dir = spanId.replace(/\./g,"/") + "/";
     /* var file = textId.split(".")[textId.split(".").length-1];
     var url = endpoint + dir + file + ".txt"; */
     var url = endpoint + dir + textId + ".txt";
-    /* console.log(url); */
     var xhttp;
     xhttp=new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById(textId).innerText = xhttp.responseText;
+            var text = document.createElement("span");
+            text.innerHTML = xhttp.responseText;
+            text.setAttribute("id",spanId+".txt");
+            text.setAttribute("class","text");
+            /* document.getElementById(spanId).parentElement.innerHTML += text; */
+            document.getElementById(spanId).parentElement.appendChild(text);
+            /* document.getElementById(spanId).innerHTML += xhttp.responseText; */
         }
     };
     xhttp.open("GET", url, true);
     xhttp.overrideMimeType("text/plain");
     xhttp.send();
+}
+
+function showText(element) {
+    element.removeAttribute("onclick");
+    document.getElementById("edition").onclick = function() {window.location.href = "./"};
+    var span = element.parentElement;
+    var parent = span.parentElement;
+    parent.innerHTML = '\n';
+    span.innerHTML += '\n\n';
+    parent.innerHTML += span.outerHTML;
+    loadTextContent(element.id);
 }
